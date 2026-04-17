4435b67e7e6fa1a9a73b760f452ad4355f883a88
Farhan Nomani <farhannomani@192.168.1.7>
2026-04-17 23:51:09 +0530

    Added sql setup files

---
+++ src/main/resources/sql/setup.sql
@@ -0,0 +1,60 @@
+USE `order_management`;
+
+DROP TABLE IF EXISTS `authorities`;
+DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `product`;
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+                         `username` varchar(50) NOT NULL,
+                         `password` varchar(68) NOT NULL,
+                         `enabled` tinyint NOT NULL,
+                         PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Inserting data for table `users`
+--
+
+INSERT INTO `users`
+VALUES
+    ('farhan','{noop}nomani',1),
+    ('winnie','{noop}chugtai',1),
+    ('rayan','{noop}deens',1);
+
+
+--
+-- Table structure for table `authorities`
+--
+
+CREATE TABLE `authorities` (
+                               `username` varchar(50) NOT NULL,
+                               `authority` varchar(50) NOT NULL,
+                               UNIQUE KEY `authorities_idx_1` (`username`,`authority`),
+                               CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Inserting data for table `authorities`
+--
+
+INSERT INTO `authorities`
+VALUES
+    ('farhan','ROLE_ADMIN'),
+    ('winnie','ROLE_USER'),
+    ('rayan','ROLE_USER'),
+    ('farhan','ROLE_USER');
+
+CREATE TABLE `product` (
+                           `id` int NOT NULL AUTO_INCREMENT,
+                           `name` varchar(255) NOT NULL,
+                           `description` varchar(1000) DEFAULT NULL,
+                           `price` decimal(10,2) NOT NULL,
+                           `stock` int NOT NULL,
+                           PRIMARY KEY (`id`),
+                           UNIQUE KEY `id_UNIQUE` (`id`)
+)
+
